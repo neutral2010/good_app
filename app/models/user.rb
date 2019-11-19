f870f5399167a901d4b2f80045e8cc6465cafd73
吉本かおり <kasiwamoti5@gmail.com>
2019-11-19 22:20:21 +0900

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.emall = emall.downcase }
+  validate :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :emall, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMALL_REGEX },
+                    uniqueness: { case_sensitive: false },
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6 }
 end
