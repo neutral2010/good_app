70fa9f4b93c008a018dbe537c37e22a266c4ea89
吉本かおり <kasiwamoti5@gmail.com>
2019-11-22 13:45:25 +0900

    再々Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   validates :emall, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMALL_REGEX },
                     uniqueness: { case_sensitive: false },
-	has _secure _password
+	has_secure_password
   validates :password, presence: true, length: {minimum: 6 }
 end
