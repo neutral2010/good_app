52da4db519ba249b1c50ba548c9a53b1c9c1f421
吉本かおり <kasiwamoti5@gmail.com>
2019-11-23 11:45:57 +0900

    railstutorial code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,20 @@
-class User < ApplicationRecord
-  before_save { self.emall = emall.downcase }
-  validate :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :emall, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMALL_REGEX },
-                    uniqueness: { case_sensitive: false },
-  has_secure_password
-  validates :password, presence: true, length: {minimum: 6 }
-end
-
-
 # class User < ApplicationRecord
-#   before_save { self.email = email.downcase }
-#   validates :name, presence: true, length: { maximum: 50 }
+#   before_save { self.emall = emall.downcase }
+#   validate :name, presence: true, length: { maximum: 50 }
 #   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-#   validates :email, presence: true, length: { maximum: 255 },
-#                     format: { with: VALID_EMAIL_REGEX },
-#                     uniqueness: { case_sensitive: false }
+#   validates :emall, presence: true, length: { maximum: 255 }, format: { with: VALID_EMALL_REGEX }, uniqueness: { case_sensitive: false },
 #   has_secure_password
-#   validates :password, presence: true, length: { minimum: 6 }
+#   validates :password, presence: true, length: {minimum: 6 }
 # end
+
+
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
