705e1d8b604f7a626fffd596b6d730a680b31dd8
吉本かおり <kasiwamoti5@gmail.com>
2019-11-29 22:16:13 +0900

    10.1ユーザー更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,28 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Smple App!"
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
 
-private
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end  
+  end
+
+  private
 
   def user_params
    params.require(:user).permit(:name, :email, :password, 
